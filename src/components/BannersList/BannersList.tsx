3646b1a98ab0d850076f3376fc2c1cd9cd76c1ed
DilSan <norms777666@gmail.com>
2024-10-15 00:06:31 +0300

    migrate from js to ts

--- src/components/BannersList/BannersList.tsx
+++ src/components/BannersList/BannersList.tsx
@@ -1,17 +1,27 @@
 import withSkeleton from "../../helpers/hocs/withSkeleton";
+import { INews } from "../../interfaces";
 import NewsBanner from "../NewsBanner/NewsBanner";
 import styles from "./styles.module.css";
 
-const BannersList = ({ banners }) => {
+interface Props {
+  banners?: INews[] | null;
+}
+
+const BannersList = ({ banners }: Props) => {
   return (
     <ul className={styles.banners}>
-      {banners.map((banner, index) => {
+      {banners?.map((banner, index) => {
         return <NewsBanner key={index} item={banner} />;
       })}
     </ul>
   );
 };
 
-const BannersListWithSkeleton = withSkeleton(BannersList, "banner", 10, "row");
+const BannersListWithSkeleton = withSkeleton<Props>(
+  BannersList,
+  "banner",
+  10,
+  "row"
+);
 
 export default BannersListWithSkeleton;
